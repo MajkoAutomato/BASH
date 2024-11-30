7d9cdc0bd4a1a6a304f6ad987fb04d0070d182c2
Maik Lischka <126296940+MajkoAutomato@users.noreply.github.com>
2024-11-30 22:39:42 +0100

    Update tar_BACKUP_ARCHIVE.sh

--- tar_BACKUP_ARCHIVE/tar_BACKUP_ARCHIVE.sh
+++ tar_BACKUP_ARCHIVE/tar_BACKUP_ARCHIVE.sh
@@ -14,6 +14,8 @@ mkdir -p ~/BASH/tar_BACKUP_ARCHIVE
 
 
 # Change to the backup archive directory
+# Command compounds is bash's programming construct 
+# Each compound command starts with reserved words and is terminated by corresponding reserved words
 
 if [ -d ~/BASH/tar_BACKUP_ARCHIVE ]; then
   cd ~/BASH/tar_BACKUP_ARCHIVE
@@ -24,11 +26,15 @@ fi
 
 
 # Create tar_BACKUP_ARCHIVE
+# -c = create , -v = verbose mode lists files being processed  , -f = specifies the file name 
 
 tar -cvf ~/BASH/tar_BACKUP_ARCHIVE/my_backup_"$(date +%d-%m-%Y_%H-%M-%S)".tar ~/* 2>/dev/null
 
 
 # Confirm completion
+# if-else condition
+# $? = variable holds the exit status assigned to (0) for success status
+# exit code (1) is assigned to 1 for failed status
 
 if [ $? -eq 0 ]; then
   echo "tar_BACKUP_ARCHIVE successfully created"
